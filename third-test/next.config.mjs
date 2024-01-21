2ba9b248f7e9e16fe8e0b71102bd1c86e326de8e
kadeem-lewis <email@kadeemlewis.com>
2024-01-21 08:00:53 -0500

    added additional image source

--- third-test/next.config.mjs
+++ third-test/next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "static.wikia.nocookie.net",
-            port: "",
-            pathname: "/warframe/images/**",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
+        port: "",
+        pathname: "/warframe/images/**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.warframestat.us",
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
