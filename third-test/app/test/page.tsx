9d0239126bdcee078c8f4c05f17822ef7bba7b45
kadeem-lewis <email@kadeemlewis.com>
2024-01-21 08:01:13 -0500

    recreated test page using api instead of package

---
+++ third-test/app/test/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+async function getWarframes() {
+  const res: any = await fetch("https://api.warframestat.us/warframes");
+  return res.json();
+}
+
+export default async function Home() {
+  const items: any = await getWarframes();
+  const warframes = items.filter(
+    (item: any) => item.isPrime !== true && item.productCategory === "Suits"
+  );
+  console.log(warframes);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {warframes.map((warframe: any) => (
+        <div key={warframe.name} className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold">{warframe.name}</h1>
+          <Image
+            src={`https://cdn.warframestat.us/img/${warframe.imageName}`}
+            alt="Warframe"
+            width={200}
+            height={200}
+          />
+          <div>{warframe.sex}</div>
+          <div>Release Year: {warframe.releaseDate.split("-")[0]}</div>
+          <div>Health: {warframe.health}</div>
+          <div>Shield: {warframe.shield}</div>
+          <div>{warframe.marketCost}</div>
+        </div>
+      ))}
+    </main>
+  );
+}
