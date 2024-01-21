4605ea4927164786624126092180523338fd71aa
kadeem-lewis <email@kadeemlewis.com>
2024-01-21 06:13:15 -0500

    testing warframe items in nitro api route

--- second-test/routes/index.ts
+++ second-test/routes/index.ts
@@ -1,6 +1,6 @@
 import Items from "warframe-items";
 
 export default eventHandler(() => {
-  const items = new Items({ category: ["Arcanes"] });
+  const items = new Items({ category: ["Warframes"] });
   return { items };
 });
