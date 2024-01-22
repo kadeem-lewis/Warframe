da7471a65020c892e0e1df2779264b905a820c80
kadeem-lewis <email@kadeemlewis.com>
2024-01-21 21:02:42 -0500

    prettier styling changes

--- third-test/app/page.tsx
+++ third-test/app/page.tsx
@@ -1,15 +1,23 @@
+import WarframeCombobox from "@/components/WarframeCombobox";
 import Image from "next/image";
 import Items from "warframe-items";
 
 export default function Home() {
   const items = new Items({ category: ["Warframes"] });
-  const warframes = Array.from(items).filter((item) => item.isPrime!==true && item.productCategory==="Suits");
+  const warframes = Array.from(items).filter(
+    (item) => item.isPrime !== true && item.productCategory === "Suits"
+  );
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {warframes.map((warframe) => (
         <div key={warframe.name} className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">{warframe.name}</h1>
-          <Image src={warframe.wikiaThumbnail} alt="Warframe" width={200} height={200}/>
+          <Image
+            src={warframe.wikiaThumbnail}
+            alt="Warframe"
+            width={200}
+            height={200}
+          />
           <div>{warframe.sex}</div>
           <div>Release Year: {warframe.releaseDate.split("-")[0]}</div>
           <div>Health: {warframe.health}</div>
@@ -17,6 +25,7 @@ export default function Home() {
           <div>{warframe.marketCost}</div>
         </div>
       ))}
+      <WarframeCombobox warframes={warframes} />
     </main>
   );
 }
